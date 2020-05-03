e12af09e1c6eddf38bd3cec5892e66ffb84bc084
Viresh J <blockchaindev@eisenhower.local>
2020-05-02 22:43:42 -0500

    fixed the typo

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -4,6 +4,6 @@ import 'source-map-support/register'
 export const handler: S3Handler = async (event: S3Event) => {
     for (const record of event.Records) {
         const key = record.s3.object.key
-        console.log('Processing S3 item with ket: ', key)
+        console.log('Processing S3 item with key: ', key)
     }
 }
