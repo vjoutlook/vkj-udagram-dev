477f476aaca4ac76d83a36b72b5c07efdf033c7a
Viresh J <blockchaindev@eisenhower.local>
2020-05-02 20:07:10 -0500

    fixing the name of url expir const

--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -63,7 +63,7 @@ function getUploadUrl(imageId: string) {
   return s3.getSignedUrl('putObject', {
     Bucket: bucketName,
     Key: imageId,
-    Expires: urlExpiration
+    Expires: Number(urlExpiration)
   })
 }
 
